f5d68e2d271c745aba13084427b9a7410fb662e4
aklein <aklein@student.hive.fi>
2025-04-23 02:09:31 +0300

    feat: add FTP service with vsftpd configuration and entrypoint script

---
+++ srcs/bonus/ftp/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -euo pipefail
+
+# ---------- 0. defaults ----------
+: "${MIN_PORT:=21000}"
+: "${MAX_PORT:=21010}"
+: "${WP_PATH:=/var/www/html}"
+: "${FTP_USER:=ftpuser}"
+
+echo "pasv_min_port=21000" >> /etc/vsftpd.conf
+echo "pasv_max_port=21010" >> /etc/vsftpd.conf
+
+adduser -D -G www-data -h ${WP_PATH} -s /sbin/nologin ${FTP_USER}
+echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
+
+exec "$@"
